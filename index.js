48d661604fbbf6a3ae196b1cc9d5ea5e9b99d103
Elliot Plant <elliotaplant@gmail.com>
2016-05-22 15:58:14 -0700

    Add cloud constructor

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function init() {
 
 	// add the objects
 	// createPlane();
-	// createSea();
+	createSea();
 	// createSky();
 
 	// start a loop that will update the objects' positions
@@ -143,7 +143,7 @@ function createLights() {
 }
 
 // First let's define a Sea object :
-var Sea = function(){
+function Sea(){
 
 	// create the geometry (shape) of the cylinder;
 	var geom = new THREE.CylinderGeometry(
@@ -182,4 +182,44 @@ function createSea(){
 
 	// add the mesh of the sea to the scene
 	scene.add(sea.mesh);
+}
+
+function Cloud(){
+	// Create an empty container that will hold the different parts of the cloud
+	this.mesh = new THREE.Object3D();
+
+	// create a cube geometry;
+	// this shape will be duplicated to create the cloud
+	var geom = new THREE.BoxGeometry(20,20,20);
+
+	// create a material; a simple white material will do the trick
+	var mat = new THREE.MeshPhongMaterial({
+		color: Colors.white,
+	});
+
+	// duplicate the geometry a random number of times
+	var nBlocs = 3+Math.floor(Math.random()*3);
+	for (var i=0; i<nBlocs; i++ ){
+
+		// create the mesh by cloning the geometry
+		var m = new THREE.Mesh(geom, mat);
+
+		// set the position and the rotation of each cube randomly
+		m.position.x = i*15;
+		m.position.y = Math.random()*10;
+		m.position.z = Math.random()*10;
+		m.rotation.z = Math.random()*Math.PI*2;
+		m.rotation.y = Math.random()*Math.PI*2;
+
+		// set the size of the cube randomly
+		var s = .1 + Math.random()*.9;
+		m.scale.set(s,s,s);
+
+		// allow each cube to cast and to receive shadows
+		m.castShadow = true;
+		m.receiveShadow = true;
+
+		// add the cube to the container we first created
+		this.mesh.add(m);
+	}
 }
