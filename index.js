f4b776f157c6b71aebcfa9ae86427dceb43d955d
Elliot Plant <elliotaplant@gmail.com>
2016-05-22 16:16:34 -0700

    Add engine and cabin to AirPlane

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ function Cloud(){
 
 	// duplicate the geometry a random number of times
 	var nBlocs = 3+Math.floor(Math.random()*3);
-	for (var i=0; i<nBlocs; i++ ){
+	for (var i = 0; i < nBlocs; i++ ){
 
 		// create the mesh by cloning the geometry
 		var m = new THREE.Mesh(geom, mat);
@@ -222,4 +222,83 @@ function Cloud(){
 		// add the cube to the container we first created
 		this.mesh.add(m);
 	}
+}
+
+// Define a Sky Object
+function Sky(){
+	// Create an empty container
+	this.mesh = new THREE.Object3D();
+
+	// choose a number of clouds to be scattered in the sky
+	this.nClouds = 20;
+
+	// To distribute the clouds consistently,
+	// we need to place them according to a uniform angle
+	var stepAngle = Math.PI*2 / this.nClouds;
+
+	// create the clouds
+	for(var i = 0; i < this.nClouds; i++){
+		var c = new Cloud();
+
+		// set the rotation and the position of each cloud;
+		// for that we use a bit of trigonometry
+		var a = stepAngle*i; // this is the final angle of the cloud
+		var h = 750 + Math.random()*200; // this is the distance between the center of the axis and the cloud itself
+
+		// Trigonometry!!! I hope you remember what you've learned in Math :)
+		// in case you don't:
+		// we are simply converting polar coordinates (angle, distance) into Cartesian coordinates (x, y)
+		c.mesh.position.y = Math.sin(a)*h;
+		c.mesh.position.x = Math.cos(a)*h;
+
+		// rotate the cloud according to its position
+		c.mesh.rotation.z = a + Math.PI/2;
+
+		// for a better result, we position the clouds
+		// at random depths inside of the scene
+		c.mesh.position.z = -400-Math.random()*400;
+
+		// we also set a random scale for each cloud
+		var s = 1+Math.random()*2;
+		c.mesh.scale.set(s,s,s);
+
+		// do not forget to add the mesh of each cloud in the scene
+		this.mesh.add(c.mesh);
+	}
+}
+
+// Now we instantiate the sky and push its center a bit
+// towards the bottom of the screen
+function createSky(){
+	var sky = new Sky();
+	sky.mesh.position.y = -600;
+	scene.add(sky.mesh);
+}
+
+function AirPlane() {
+
+	this.mesh = new THREE.Object3D();
+
+	// Create the cabin
+	var geomCockpit = new THREE.BoxGeometry(60,50,50,1,1,1); // need last three params?
+	var matCockpit = new THREE.MeshPhongMaterial({
+    color:Colors.red,
+    shading:THREE.FlatShading,
+  });
+	var cockpit = new THREE.Mesh(geomCockpit, matCockpit);
+	cockpit.castShadow = true;
+	cockpit.receiveShadow = true;
+	this.mesh.add(cockpit);
+
+  // Create the engine
+  var geomEngine = new THREE.BoxGeometry(20,50,50,1,1,1); // need last three params?
+  var matEngine = new THREE.MeshPhongMaterial({
+    color:Colors.white,
+    shading:THREE.FlatShading
+  });
+  var engine = new THREE.Mesh(geomEngine, matEngine);
+  engine.position.x = 40;
+  engine.castShadow = true;
+  engine.receiveShadow = true;
+  this.mesh.add(engine);
 }
